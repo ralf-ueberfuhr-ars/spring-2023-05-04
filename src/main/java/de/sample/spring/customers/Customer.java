3fa4bde65d3e8b94b471409716dad4337c43205b
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-05-05 15:39:54 +0200

    Add validation for customer

--- src/main/java/de/sample/spring/customers/Customer.java
+++ src/main/java/de/sample/spring/customers/Customer.java
@@ -2,6 +2,7 @@ package de.sample.spring.customers;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -24,6 +25,7 @@ public class Customer {
       description = "The simple name.",
       example = "Tom Mayer"
     )
+    @Size(min = 3)
     private String name;
 
     @Schema(
