31cc3a3a1bdda712258a19184d5e420216ddcd1a
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-05-05 11:51:56 +0200

    Add Swagger Annotations (finish)

--- src/main/java/de/sample/spring/customers/Customer.java
+++ src/main/java/de/sample/spring/customers/Customer.java
@@ -1,17 +1,34 @@
 package de.sample.spring.customers;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
+@Schema(
+  name="Customer",
+  description = "Information about the customer."
+)
 public class Customer {
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Schema(
+      description = "The unique identifier."
+    )
     private UUID uuid;
+
+    @Schema(
+      description = "The simple name.",
+      example = "Tom Mayer"
+    )
     private String name;
+
+    @Schema(
+      description = "The date of birth."
+    )
     private LocalDate birthdate;
 
 }
--- src/main/java/de/sample/spring/customers/CustomerController.java
+++ src/main/java/de/sample/spring/customers/CustomerController.java
@@ -1,10 +1,14 @@
 package de.sample.spring.customers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,23 +30,53 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("api/v1/customers")
-@Tag(name="customers")
+@Tag(name = "customers")
 public class CustomerController {
 
     // TODO replace later
     private final Map<UUID, Customer> customers = new HashMap<>();
 
-    @GetMapping
+    @GetMapping(
+      produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Read all customers")
+    @ApiResponse(
+      responseCode = "200",
+      description = "Customers were read successfully."
+    )
     public Collection<Customer> findAllCustomers() {
         return customers.values();
     }
 
-    @GetMapping("/{id}")
-    public Customer findCustomer(@PathVariable("id") UUID id) {
+    @GetMapping(
+      value = "/{id}",
+      produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Read a single customers")
+    @ApiResponse(
+      responseCode = "200",
+      description = "Customer was found."
+    )
+    @ApiResponse(
+      responseCode = "404",
+      description = "Customer could not be found.",
+      content = @Content(schema = @Schema)
+    )
+    public Customer findCustomer(
+      @PathVariable("id")
+      @Parameter(
+        description = "The unique identifier",
+        example = "8fd1c43c-3aa6-47e3-a1c6-bda0d7f32409"
+      )
+      UUID id
+    ) {
         return customers.get(id);
     }
 
-    @PostMapping
+    @PostMapping(
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE
+    )
     /*
      * Beschreibungen
      * Statuscode 201, Location Header
@@ -59,7 +93,8 @@ public class CustomerController {
     )
     @ApiResponse(
       responseCode = "400",
-      description = "The customer is invalid."
+      description = "The customer is invalid.",
+      content = @Content(schema = @Schema)
     )
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer newCustomer) {
         var newId = UUID.randomUUID();
@@ -74,7 +109,23 @@ public class CustomerController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteCustomer(@PathVariable("id") UUID id) {
+    @Operation(summary = "Delete a single customers")
+    @ApiResponse(
+      responseCode = "204",
+      description = "Customer was deleted successfully."
+    )
+    @ApiResponse(
+      responseCode = "404",
+      description = "Customer could not be found."
+    )
+    public void deleteCustomer(
+      @PathVariable("id")
+      @Parameter(
+        description = "The unique identifier",
+        example = "8fd1c43c-3aa6-47e3-a1c6-bda0d7f32409"
+      )
+      UUID id
+    ) {
         customers.remove(id);
     }
 
