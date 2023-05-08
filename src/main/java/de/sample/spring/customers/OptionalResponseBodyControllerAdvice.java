39fdb4af2ef359ecf0c7d3756f0534c5e6be00ef
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-05-08 11:32:46 +0200

    Introduce ResponseBodyAdvice for NotFound code

--- src/main/java/de/sample/spring/customers/OptionalResponseBodyControllerAdvice.java
+++ src/main/java/de/sample/spring/customers/OptionalResponseBodyControllerAdvice.java
@@ -2,7 +2,6 @@ package de.sample.spring.customers;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
@@ -12,7 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import java.util.Optional;
 
 @RestControllerAdvice
-public class OptionalResponseBodyControllerAdvice implements ResponseBodyAdvice<Object> {
+public class OptionalResponseBodyControllerAdvice implements ResponseBodyAdvice<Optional<?>> {
 
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
@@ -20,9 +19,11 @@ public class OptionalResponseBodyControllerAdvice implements ResponseBodyAdvice<
     }
 
     @Override
-    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        return body instanceof Optional optional
-          ? optional.orElseGet(ResponseEntity.notFound()::build)
-          : body;
+    public Optional<?> beforeBodyWrite(Optional<?> body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if(body.isPresent()) {
+            return body;
+        } else {
+            throw new NotFoundException();
+        }
     }
 }
